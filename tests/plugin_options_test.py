b439aff53c2eeb42cdafc1d35ea9e081d6aa4bdc
Mike <jabbera@users.noreply.github.com>
2023-02-06 16:39:26 -0500

    Support flake8 v6 (#46)
    * Support flake8 v6
    
    * Update Changelog
    
    * Update CHANGELOG
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Slight restructuring of option manager test code
    
    * Fix flake8 warning
    
    ---------
    
    Co-authored-by: Mike Barry <michael.barry@gmo.com>
    Co-authored-by: Nguyen Damien <ngn.damien@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/plugin_options_test.py
+++ tests/plugin_options_test.py
@@ -35,6 +35,13 @@ import pytest
 import flake8_secure_coding_standard as flake8_scs
 
 
+def create_options_manager():
+    ctor_args = {'version': '1.0', 'plugin_versions': '', 'parents': []}
+    if int(flake8.__version__[0]) >= 6:
+        ctor_args['formatter_names'] = []
+    return flake8.options.manager.OptionManager(**ctor_args)
+
+
 def results(s):
     return {'{}:{}: {}'.format(*r) for r in flake8_scs.Plugin(ast.parse(s)).run()}
 
@@ -107,7 +114,7 @@ def test_read_octal_mode_option_invalid(arg):
     ids=_id_func,
 )
 def test_os_allowed_mode(function, arg, allowed_modes):
-    options = flake8.options.manager.OptionManager(version='1.0', plugin_versions="", parents=[])
+    options = create_options_manager()
     flake8_scs.Plugin.add_options(options)
     flake8_scs.Plugin.parse_options(options.parse_args([f'--os-{function}-mode={arg}']))
     assert getattr(flake8_scs.Visitor, f'os_{function}_modes_allowed') == allowed_modes
